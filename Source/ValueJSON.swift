5f0e8baa7998c812f8974156930d793e527df8c2
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-01 22:59:17 -0500

    added iOS target

--- Source/ValueJSON.swift
+++ Source/ValueJSON.swift
@@ -5,17 +5,26 @@
 //  Created by Alsey Coleman Miller on 7/23/15.
 //  Copyright © 2015 PureSwift. All rights reserved.
 //
-/*
+
+import SwiftFoundation
+
 /// Converts the values object to JSON
-public func ConvertValuesToJSON(value: ValuesObject) -> JSONObject {
+public extension Entity {
     
-    
+    /// Converts ```JSON``` to *CoreModel* values. 
+    ///
+    /// - returns: The converted values or ```nil``` if the provided values do not match the entity's properties.
+    func convert(values: JSONObject) -> ValuesObject? {
+        
+        return nil
+    }
 }
 
-/// Attempts to convert the JSON object to a values object.
-public func ConvertJSONToValues(value: JSONObject) -> ValuesObject? {
+public extension JSON {
     
-    
+    /// Converts **CoreModel** values to ```JSON```.
+    static func fromValues(values: ValuesObject) -> JSONObject {
+        
+        return JSONObject()
+    }
 }
-
-*/
