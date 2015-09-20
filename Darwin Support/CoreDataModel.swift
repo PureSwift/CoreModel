a1819e51bc98d51957d54e9bbd7e8735ec57d764
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-20 14:52:21 -0500

    fixed CoreData support for relationship

--- Darwin Support/CoreDataModel.swift
+++ Darwin Support/CoreDataModel.swift
@@ -122,7 +122,7 @@ public extension NSRelationshipDescription {
         
         guard let inverseRelationshipName = self.inverseRelationship?.name else { return nil }
         
-        return Relationship(type: RelationshipType(toMany: self.toMany), destinationEntityName: destinationEntityName, inverseRelationshipName: inverseRelationshipName)
+        return Relationship(type: RelationshipType(toMany: self.toMany), optional: self.optional, destinationEntityName: destinationEntityName, inverseRelationshipName: inverseRelationshipName)
     }
 }
 
