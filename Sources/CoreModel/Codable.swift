746def124f01a07b5b43bf8faa734b66e58a1375
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-19 01:39:29 -0400

    Add `AttributeCodable`

---
+++ Sources/CoreModel/Codable.swift
@@ -0,0 +1,10 @@
+//
+//  Codable.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/18/23.
+//
+
+import Foundation
+
+public typealias AttributeCodable = AttributeEncodable & AttributeDecodable
