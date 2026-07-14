ae8049ea10191c5bddb06647dd8219eef51eabd2
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-13 22:31:07 -0400

    Gate Relationship and RelationshipType Codable conformances behind hasFeature(Embedded)

--- Sources/CoreModel/Relationship.swift
+++ Sources/CoreModel/Relationship.swift
@@ -5,24 +5,22 @@
 //  Created by Alsey Coleman Miller on 8/16/23.
 //
 
-import Foundation
-
 /// CoreModel `Relationship`
-public struct Relationship: Property, Codable, Equatable, Hashable, Identifiable, Sendable {
-    
+public struct Relationship: Property, Equatable, Hashable, Identifiable, Sendable {
+
     public let id: PropertyKey
-    
+
     public var type: RelationshipType
-    
+
     public var destinationEntity: EntityName
-    
+
     public var inverseRelationship: PropertyKey
-    
+
     public init(id: PropertyKey,
                 type: RelationshipType,
                 destinationEntity: EntityName,
                 inverseRelationship: PropertyKey) {
-        
+
         self.id = id
         self.type = type
         self.destinationEntity = destinationEntity
@@ -31,8 +29,15 @@ public struct Relationship: Property, Codable, Equatable, Hashable, Identifiable
 }
 
 /// CoreModel `Relationship` Type
-public enum RelationshipType: String, Codable, CaseIterable, Sendable {
-    
+public enum RelationshipType: String, CaseIterable, Sendable {
+
     case toOne
     case toMany
 }
+
+// MARK: - Codable
+
+#if !hasFeature(Embedded)
+extension Relationship: Codable {}
+extension RelationshipType: Codable {}
+#endif
