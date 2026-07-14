1b97868904ddabbae4ca3143f7e632be2f58a3ca
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-13 22:31:47 -0400

    Gate CodingUserInfoKey extension behind hasFeature(Embedded)

--- Sources/CoreModel/Extensions/CodingUserInfoKey.swift
+++ Sources/CoreModel/Extensions/CodingUserInfoKey.swift
@@ -5,20 +5,20 @@
 //  Created by Alsey Coleman Miller on 8/19/23.
 //
 
-import Foundation
-
+#if !hasFeature(Embedded)
 public extension CodingUserInfoKey {
-    
+
     init(_ key: ModelCodingUserInfoKey) {
         self.init(rawValue: key.rawValue)!
     }
-    
+
     static var identifierCodingKey: CodingUserInfoKey {
         .init(.identifierCodingKey)
     }
 }
 
 public enum ModelCodingUserInfoKey: String {
-    
+
     case identifierCodingKey = "org.pureswift.CoreModel.CodingUserInfoKey.identifierCodingKey"
 }
+#endif
